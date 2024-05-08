918bfa942365632f1b80702107c2467b4ff459ba
rGuszz <gusrodriguezayala@gmail.com>
2024-05-08 17:42:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ with titulo:
 
     st.sidebar.header("Filtro")
     empresa = st.sidebar.multiselect("Elije una opción", options=df["Name"], max_selections=1, default="Apple Inc. Common Stock")
-    activo = df[df["Name"] == empresa]["Symbol"]
+    st.write(empresa)
+    activo = "AAPL"
+    #activo = df[df["Name"] == empresa]["Symbol"]
 
 with datos:
     st.header("Precios del activo y gráfica")
