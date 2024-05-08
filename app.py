b4d32a61742a16bf05bde7658123477c9cbc5de1
rGuszz <gusrodriguezayala@gmail.com>
2024-05-08 15:52:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ opciones = st.container()
 
 with titulo:
     st.title("Bienvenido a la interfaz para calcular el precio de opciones y ver otros datos")
-    tickers = pd.read_csv(r'C:\Users\gusta\OneDrive\Escritorio\Streamlit\nasdaq_screener_1715204451275.csv', usecols=["Symbol"])
+    tickers = pd.read_csv(r'', usecols=["Symbol"])
     st.dataframe(tickers)
     activo = st.text_input("Introduce el nombre del activo", value="AAPL") 
 
