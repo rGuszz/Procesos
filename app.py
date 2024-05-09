86e1cadfbdc3f4488a800037b0cb6ef5c4992c44
rGuszz <gusrodriguezayala@gmail.com>
2024-05-08 18:01:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,9 @@ with titulo:
     st.write(df)
 
     st.sidebar.header("Filtro")
-    empresa = st.sidebar.multiselect("Elije una opción", options=df["Name"], max_selections=1, default="Apple Inc. Common Stock", key="str")
-    activo = df.loc[empresa]["Symbol"]
+    empresa = st.sidebar.multiselect("Elije una opción", options=df["Name"], max_selections=1, default="Apple Inc. Common Stock")
+    e = str(empresa)
+    activo = df.loc[e]["Symbol"]
     st.write(activo)
     
 with datos:
